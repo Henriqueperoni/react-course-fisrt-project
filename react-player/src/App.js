ada848c3e6567d8be2852b35eef190a39bd32255
Henriqueperoni <henriqueperoni@hotmail.com>
2021-01-11 20:57:07 +0000

    Add library toggle

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -5,6 +5,7 @@ import "./styles/app.scss";
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library'
+import Nav from './components/Nav'
 // Import Data
 import data from "./util"
 
@@ -19,6 +20,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration
@@ -26,9 +28,10 @@ function App() {
   };
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player audioRef={audioRef} isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} setSongInfo={setSongInfo} songInfo={songInfo} />
-      <Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} />
+      <Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} setSongs={setSongs} libraryStatus={libraryStatus} />
       <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
--- react-player/src/components/Library.js
+++ react-player/src/components/Library.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import LibrarySong from './LibrarySong'
 
-const Library = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
+const Library = ({ songs, setCurrentSong, audioRef, isPlaying, setSongs, libraryStatus }) => {
   return (
-    <div className="library">
+    <div className={`library ${libraryStatus ? 'active-library' : ""}`} >
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map(song => (
-          <LibrarySong song={song} setCurrentSong={setCurrentSong} songs={songs} id={song.id} key={song.id} audioRef={audioRef} />))}
+          <LibrarySong song={song} setCurrentSong={setCurrentSong} songs={songs} id={song.id} key={song.id} audioRef={audioRef} setSongs={setSongs} />))}
       </div>
-    </div>
+    </div >
   )
 }
 
